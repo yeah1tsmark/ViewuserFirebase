1d81a56bace7d800a964822d07bcfe2377e2dfd5
yeah1tsmark <marcwafula635@gmail.com>
2023-05-12 11:01:25 +0300

    added more display and view data

--- app/src/main/java/com/example/displayfirebase/ViewUsers.kt
+++ app/src/main/java/com/example/displayfirebase/ViewUsers.kt
@@ -2,10 +2,48 @@ package com.example.displayfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import android.widget.Toast
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
 
 class ViewUsers : AppCompatActivity() {
+    lateinit var mylistview:ListView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_users)
+
+        mylistview = findViewById(R.id.mListPeople)
+
+        var users:ArrayList<User> = ArrayList()
+
+        var myadapter = CustomAdapter(applicationContext, users)
+
+        var mydb = FirebaseDatabase.getInstance().reference.child("Names")
+
+        mydb.addValueEventListener(object : ValueEventListener{
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //get data and display in array
+                users.clear()
+                for (snap in snapshot.children){
+                    var person = snap.getValue(User::class.java)
+                    users.add(person!!)
+                }
+
+                myadapter.notifyDataSetChanged()
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(applicationContext, "Failed to Display Data", Toast.LENGTH_SHORT).show()
+
+            }
+
+        })
     }
 }
